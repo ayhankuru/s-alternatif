96455bbd302e248d31956bf3349d798da432a8c2
cobaimelan <cobaimelan@yandex.com.tr>
2014-07-28 18:50:51 +0300

    travis test

--- test.js
+++ test.js
@@ -11,7 +11,6 @@ describe('Search Test!!!...', function(){
 			expect(data.lyrics[0].id).to.eql(21805);
 			expect(data.lyrics[0].title).to.eql('Afili Yalnızlık');
 			done();
-			console.log(data);
 		});
 	});
 	it('Show Test :1 ', function(done){
@@ -21,7 +20,6 @@ describe('Search Test!!!...', function(){
 			expect(data.title).to.eql('Emre Aydın - Afili Yalnızlık');
 			expect(data.body).to.eql('\r\nÖlsem (ölsem), ölsem (ölsem), ölsem... hemen şimdi \r\nKaçsam (kaçsam), gitsem (gitsem), kaçsam... tam da şimdi \r\n\r\nBu kez pek bir afili yalnızlık \r\nAldatan bir kadın kadar düşman \r\nAğzı bozuk üstelik bırakmıyor acıtmadan \r\nBu kez pek bir afili yalnızlık \r\nAğlayan bir kadın kadar düşman \r\nTuzaklar kurmuş üstelik \r\nBırakmıyor acıtmadan \r\n\r\nBitiyorum her nefeste \r\nNe halim varsa gördüm \r\nÇok koştum, çok yoruldum \r\nVe şimdi ben de düştüm...\r\nx2\r\n\r\n\r\nSövdüm (sövdüm), sövdüm (sövdüm), sövdüm ben dünyaya \r\nAcılara, sokaklara, ait olmaya, insanlara \r \n\r\nBu kez pek bir afili yalnızlık \r\nAldatan bir kadın kadar düşman \r\nAğzı bozuk üstelik bırakmıyor acıtmadan \r\nBu kez pek bir afili yalnızlık \r\nAğlayan bir kadın kadar düşman \r\nTuzaklar kurmuş üstelik \r\nBırakmıyor acıtmadan \r\n\r\nBitiyorum her nefeste \r\nNe halim varsa gördüm \r\nÇok koştum, çok yoruldum \r\nVe şimdi ben de düştüm...\r\nx2\r\n\r\nDeğmezmiş hiç uğraşmaya\r\nBu kez mecalim yok hiç dayanmaya.. dayanmaya..\r\n\r\nBitiyorum her nefeste \r\nNe halim varsa gördüm \r\nÇok koştum, çok yoruldum \r\nVe şimdi ben de düştüm...\r\nx2 \r\n\n\r\n\r\n \r\n\r\n\r\n \r\n\r\n\r\n');
 			done();
-			console.log(data);
 		});
 	});
 });
